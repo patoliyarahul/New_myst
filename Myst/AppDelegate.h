bf78c2abf7ad8247db567a1321160125efa56581
Vipul Jikadra <JD@vipuls-mac-mini.local>
2016-12-08 15:56:25 +0530

    Initial Commit

---
+++ Myst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myst
+//
+//  Created by Vipul Jikadra on 08/12/16.
+//  Copyright © 2016 Vipul Jikadra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Myst
+//
+//  Created by Vipul Jikadra on 08/12/16.
+//  Copyright © 2016 Vipul Jikadra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
