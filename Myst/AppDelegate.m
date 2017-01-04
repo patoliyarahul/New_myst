47e0a85207b4647e96a74a28aea54a1d51df3de2
Vipul Jikadra <JD@vipuls-mac-mini.local>
2017-01-04 19:17:22 +0530

    FCM Integration Done

--- Myst/AppDelegate.m
+++ Myst/AppDelegate.m
@@ -26,15 +26,6 @@
     
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-        // Override point for customization after application launch.
-        
-//                for (NSString *familyName in [UIFont familyNames]){
-//                    NSLog(@"Family name: %@", familyName);
-//                    for (NSString *fontName in [UIFont fontNamesForFamilyName:familyName])
-//                    {
-//                        NSLog(@"--Font name: %@", fontName);
-//                    }
-//                }
     
     [Fabric with:@[[Crashlytics class]]];
     
@@ -66,8 +57,179 @@
     
     [window makeKeyAndVisible];
     
-        return YES;
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+        UIUserNotificationType allNotificationTypes =
+        (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+        UIUserNotificationSettings *settings =
+        [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    } else {
+        // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+        UNAuthorizationOptions authOptions =
+        UNAuthorizationOptionAlert
+        | UNAuthorizationOptionSound
+        | UNAuthorizationOptionBadge;
+        [[UNUserNotificationCenter currentNotificationCenter]
+         requestAuthorizationWithOptions:authOptions
+         completionHandler:^(BOOL granted, NSError * _Nullable error) {
+         }
+         ];
+        
+        // For iOS 10 display notification (sent via APNS)
+        [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
+        // For iOS 10 data message (sent via FCM)
+        //   [[FIRMessaging messaging] setRemoteMessageDelegate:self];
+#endif
     }
+    
+    
+    [[UIApplication sharedApplication] registerForRemoteNotifications];    // [START configure_firebase]
+    [FIRApp configure];
+    // [END configure_firebase]
+    
+    // Add observer for InstanceID token refresh callback.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tokenRefreshNotification:)
+                                                 name:kFIRInstanceIDTokenRefreshNotification object:nil];
+    
+    NSString *token = [[FIRInstanceID instanceID] token];
+
+    
+        return YES;
+}
+#define iOS10 Delegate method
+
+//Called when a notification is delivered to a foreground app.
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
+    NSLog(@"User Info : %@",notification.request.content.userInfo);
+    completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+}
+
+//Called to let your app know which action was selected by the user for a given notification.
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler{
+    NSLog(@"User Info : %@",response.notification.request.content.userInfo);
+    completionHandler();
+    
+//    NSDateFormatter *formatter;
+//    NSString        *dateString;
+//    
+//    formatter = [[NSDateFormatter alloc] init];
+//    [formatter setDateFormat:@"dd-MM-yyyy hh:mm a"];
+//    
+//    dateString = [formatter stringFromDate:[NSDate date]];
+//    
+//    
+//    if ([[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"type"] integerValue] == 2 )
+//    {
+//        
+//        [[Database sharedDatabase]Insert_Record:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"type"] category_image:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"category_image"] image_path:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"image_path"] message:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"message"] category_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"Category_Id"] subcat_name:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subcat_name"] subcategory_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subCatId"] date:dateString];
+//    }
+//    else
+//    {
+//        [[Database sharedDatabase]Insert_Record:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"type"] category_image:@"" image_path:@"" message:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"title"] category_id:@"" subcat_name:@"" subcategory_id:@"" date:dateString];
+//    }
+//    
+//    UIApplication *application;
+//    
+//    UIApplicationState state = [application applicationState];
+//    if (state == UIApplicationStateActive)
+//    {
+//        if ([[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"type"] integerValue] == 2 )
+//        {
+//            [vcContainerViewController addCustomPopupNoti:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"body"] imagepath:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"image_path"] category_image:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"category_image"] category_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"Category_Id"] subcat_name:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subcat_name"] subcategory_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subCatId"] dict:[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"]];
+//        }
+//        else
+//        {
+//            [vcContainerViewController addCustomPopup:[NSString stringWithFormat:@"%@ ",[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"body"]]];
+//        }
+//        
+//    }
+//    else if (state == UIApplicationStateInactive)
+//    {
+//        if ([[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"type"] integerValue] == 2 )
+//        {
+//            [vcContainerViewController addCustomPopupNoti:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"body"] imagepath:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"image_path"] category_image:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"category_image"] category_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"Category_Id"] subcat_name:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subcat_name"] subcategory_id:[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"subCatId"] dict:[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"]];
+//        }
+//        else
+//        {
+//            [vcContainerViewController addCustomPopup:[NSString stringWithFormat:@"%@ ",[[[response.notification.request.content.userInfo valueForKey:@"aps"] valueForKey:@"alert"] valueForKey:@"body"]]];
+//        }
+//    }
+    
+}
+- (void)tokenRefreshNotification:(NSNotification *)notification
+{
+    // Note that this callback will be fired everytime a new token is generated, including the first
+    // time. So if you need to retrieve the token as soon as it is available this is where that
+    // should be done.
+    NSString *refreshedToken = [[FIRInstanceID instanceID] token];
+    NSLog(@"InstanceID token: %@", refreshedToken);
+    
+    // Connect to FCM since connection may have failed when attempted before having a token.
+    [self connectToFcm];
+    
+    // TODO: If necessary send token to application server.
+}
+- (void)connectToFcm
+{
+    [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error)
+     {
+         if (error != nil)
+         {
+             NSLog(@"Unable to connect to FCM. %@", error);
+         }
+         else
+         {
+             NSLog(@"Connected to FCM.");
+         }
+     }];
+}
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    [[FIRInstanceID instanceID] setAPNSToken:deviceToken
+                                        type:FIRInstanceIDAPNSTokenTypeSandbox];
+    NSString* newToken = [deviceToken description];
+    newToken = [newToken stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    newToken = [newToken stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"token:: %@",newToken);
+    
+    _TokenId = newToken;
+    
+//    UserInfo * ob = (UserInfo *) [obNet getUserInfoObject];
+//    if (IsObNotNil(ob))
+//    {
+//        NSMutableDictionary * mD = [NSMutableDictionary new];
+//        mD[@"Id"] = [ob valueForKey:@"Id"];
+//        mD[@"Fcm_Id"] = KAppDelegate.TokenId;
+//        
+//        [obNet JSONFromWebServices:Fcmregistration Parameter:mD Method:@"POST" AI:YES PopUP:YES Caller:CALLER WithBlock:^(id json)
+//         {
+//             if (IsObNotNil(json))
+//             {
+//                 // ToastMSG(json[@"message"]);
+//                 
+//             }
+//             
+//         }];
+//        
+//    }
+//    else
+//    {
+//        
+//    }
+}
+
+- (void)application:(UIApplication *)application
+didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"Registration for remote notification failed with error: %@", error.localizedDescription);
+    // [END receive_apns_token_error]
+    NSDictionary *userInfo = @{@"error" :error.localizedDescription};
+    [[NSNotificationCenter defaultCenter] postNotificationName:_registrationKey
+                                                        object:nil
+                                                      userInfo:userInfo];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application
     {
@@ -82,16 +244,17 @@
     }
     
 - (void)applicationWillEnterForeground:(UIApplication *)application
-    {
+{
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
+}
     
+
 - (void)applicationDidBecomeActive:(UIApplication *)application
-    {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-       
-        
-    }
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [self connectToFcm];
+    
+}
     
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
